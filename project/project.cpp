b5942e072bef9897b2842f7b7508a2004ca6b6d8
Pedro Ruano Silveira <pruanosilveira@gmail.com>
2023-11-20 02:35:50 +0000

    passing all the pub tests, needs optimization

--- project/project.cpp
+++ project/project.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
+#include <map>
+#include <tuple>
 using namespace std;
 
-int maxValue(int X, int Y, const vector<int>& a, const vector<int>& b, const vector<int>& p, int n) {
-    vector<vector<int>> dp(X + 1, vector<int>(Y + 1, 0));
-
-    for (int x = 1; x <= X; x++) {
-        for (int y = 1; y <= Y; y++) {
-            for (int k = 0; k < n; k++) {
-                if (x >= a[k] && y >= b[k]) {
-                    dp[x][y] = max(dp[x][y], dp[x - a[k]][y - b[k]] + p[k]);
-                }       
-            }
-        }
-
+// Função para calcular o valor máximo
+int dfs(int remaining_X, int remaining_Y, const vector<int>& a, const vector<int>& b, const vector<int>& p, int n, map<pair<int, int>, int>& memo) {
+    // Verifica se o resultado já foi computado
+    if (memo.find({remaining_X, remaining_Y}) != memo.end()) {
+        return memo[{remaining_X, remaining_Y}];
     }
-    return dp[X][Y];
+
+    int max_value = 0;
+    for (int i = 0; i < n; ++i) {
+        // Tenta colocar a peça na orientação original
+        if (remaining_X >= a[i] && remaining_Y >= b[i]) {
+            int value = p[i] + dfs(remaining_X - a[i], remaining_Y, a, b, p, n, memo)
+                              + dfs(a[i], remaining_Y - b[i], a, b, p, n, memo);
+            max_value = max(max_value, value);
+        }
+        // Tenta colocar a peça na orientação rotacionada
+        if (remaining_X >= b[i] && remaining_Y >= a[i]) {
+            int value = p[i] + dfs(remaining_X - b[i], remaining_Y, a, b, p, n, memo)
+                              + dfs(b[i], remaining_Y - a[i], a, b, p, n, memo);
+            max_value = max(max_value, value);
+        }
+    }
+
+    // Armazena o resultado no cache
+    memo[{remaining_X, remaining_Y}] = max_value;
+    return max_value;
+}
+
+// Função para iniciar o cálculo do valor máximo
+int maxValue(int X, int Y, const vector<int>& a, const vector<int>& b, const vector<int>& p, int n) {
+    map<pair<int, int>, int> memo;
+    return dfs(X, Y, a, b, p, n, memo);
 }
 
 int main() {
