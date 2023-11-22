81b2c965ef36b059d926d5659da13a9a8a2b135d
Pedro Ruano Silveira <pruanosilveira@gmail.com>
2023-11-22 16:09:14 +0000

    project is currently on test

--- project/test.cpp
+++ project/test.cpp
@@ -3,18 +3,15 @@
 #include <algorithm>
 using namespace std;
 
-// Função para calcular o valor máximo de maneira iterativa
 int maxValue(int X, int Y, const vector<int>& a, const vector<int>& b, const vector<int>& p, int n) {
     vector<vector<int>> dp(X + 1, vector<int>(Y + 1, 0));
 
     for (int x = 0; x <= X; x++) {
         for (int y = 0; y <= Y; y++) {
             for (int i = 0; i < n; i++) {
-                // Tenta colocar a peça na orientação original
                 if (x >= a[i] && y >= b[i]) {
                     dp[x][y] = max(dp[x][y], p[i] + dp[x - a[i]][y] + dp[a[i]][y - b[i]]);
                 }
-                // Tenta colocar a peça na orientação rotacionada
                 if (x >= b[i] && y >= a[i]) {
                     dp[x][y] = max(dp[x][y], p[i] + dp[x - b[i]][y] + dp[b[i]][y - a[i]]);
                 }
