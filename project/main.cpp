f6b4bdaa977818c7deca3f8442cee060b5bf9ddc
PedroRuanoS <pruanosilveira@gmail.com>
2023-12-04 17:30:11 +0000

    relatorio improved

--- project/main.cpp
+++ project/main.cpp
@@ -14,40 +14,41 @@
 
 using namespace std;
 
-int maxValue(vector<vector<int>>& matrix, int X, int Y) {
+int maxValue(vector<vector<int>>& allValues, int X, int Y) {
 
     for (int x = 1; x <= X; x++) {
         for (int y = 1; y <= Y; y++) {
             int maxCurrentXY = 0;
             for (int i = 1; i <= x/2; i++) {
-                maxCurrentXY = max(maxCurrentXY, matrix[i][y] + matrix[x-i][y]);
+                maxCurrentXY = max(maxCurrentXY, allValues[i][y] + allValues[x-i][y]);
             }
             for (int j = 1; j <= y/2; j++) {
-                maxCurrentXY = max(maxCurrentXY, matrix[x][j] + matrix[x][y-j]);
+                maxCurrentXY = max(maxCurrentXY, allValues[x][j] + allValues[x][y-j]);
             }
-            matrix[x][y] = max(matrix[x][y], maxCurrentXY);
+            allValues[x][y] = max(allValues[x][y], maxCurrentXY);
         }
     }
-    return matrix[X][Y];
+    
+    return allValues[X][Y];
 }
 
 int main() {
     int X, Y, n;
     scanf("%d %d %d", &X, &Y, &n);
 
-    vector<vector<int>> matrix(X + 1, vector<int>(Y + 1, 0));
+    vector<vector<int>> allValues(X + 1, vector<int>(Y + 1, 0));
 
     for (int i = 0; i < n; i++) {
         int a, b, p;
         scanf("%d %d %d", &a, &b, &p);
         if (a <= X && b <= Y) {
-            matrix[a][b] = max(matrix[a][b], p);
+            allValues[a][b] = max(allValues[a][b], p);
         }
         if (b <= X && a <= Y) {
-            matrix[b][a] = max(matrix[b][a], p);
+            allValues[b][a] = max(allValues[b][a], p);
         }
     }
 
-    printf("%d\n", maxValue(matrix, X, Y));
+    printf("%d\n", maxValue(allValues, X, Y));
     return 0;
 }
